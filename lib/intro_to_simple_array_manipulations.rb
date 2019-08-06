68ec8e36444d93af92f9c9bff477f0d9270e50bc
loading-actor-9896 <upalazzo93@gmail.com>
2019-08-06 19:48:03 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -10,15 +10,15 @@ def using_pop (array)
   array.pop 
 end 
 
-#def pop_with_args (array)
-  #array!pop
- # array.pop
-#end
+def pop_with_args (array)
+  array.pop(2)
+end
 
 def using_shift (array)
   array.shift
 end
 
 def shift_with_args (array)
-  array.shift
+  array.shift(2)
+end
   
